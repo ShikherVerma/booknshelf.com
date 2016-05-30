f6ad64748be1459dfc01ddd47886c01a3edea57a
Tigran Hakobyan <tik.hakobyan@gmail.com>
2016-05-30 10:22:15 -0700

    organize some code

--- resources/views/nav/user-right.blade.php
+++ resources/views/nav/user-right.blade.php
@@ -6,3 +6,52 @@
         </div>
     </a>
 </li>
+
+<!-- Notifications -->
+<li>
+    <a @click="showNotifications" class="has-activity-indicator">
+        <div class="navbar-icon">
+            <i class="activity-indicator" v-if="hasUnreadNotifications"></i>
+            <i class="icon fa fa-bell"></i>
+        </div>
+    </a>
+</li>
+
+<li class="dropdown">
+    <!-- User Photo / Name -->
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+        <img src="{{ Auth::user()->avatar }}" class="app-nav-profile-photo m-r-xs">
+        <span class="caret"></span>
+    </a>
+
+    <ul class="dropdown-menu" role="menu">
+        <!-- Developer -->
+        @if (Auth::user()->email == 'tik.hakobyan@gmail.com')
+            @include('nav.developer')
+        @endif
+        <li>
+            <a href="/tigran">
+                Profile
+            </a>
+        </li>
+        <li>
+            <a href="/tigran/bookshelves">
+                Bookshelves
+            </a>
+        </li>
+        <li>
+            <a href="/settings">
+                Settings
+            </a>
+        </li>
+
+        <li class="divider"></li>
+
+        <!-- Logout -->
+        <li>
+            <a href="/logout">
+                Logout
+            </a>
+        </li>
+    </ul>
+</li>
--- resources/views/nav/user.blade.php
+++ resources/views/nav/user.blade.php
@@ -24,74 +24,9 @@
                 <ul class="nav navbar-nav">
                     @includeIf('nav.user-left')
                 </ul>
-
                 <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
                     @includeIf('nav.user-right')
-
-                    <!-- Notifications -->
-                    <li>
-                        <a @click="showNotifications" class="has-activity-indicator">
-                            <div class="navbar-icon">
-                                <i class="activity-indicator" v-if="hasUnreadNotifications"></i>
-                                <i class="icon fa fa-bell"></i>
-                            </div>
-                        </a>
-                    </li>
-
-                    <li class="dropdown">
-                        <!-- User Photo / Name -->
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            <img src="{{ Auth::user()->avatar }}" class="app-nav-profile-photo m-r-xs">
-                            <span class="caret"></span>
-                        </a>
-
-                        <ul class="dropdown-menu" role="menu">
-                            <!-- Impersonation -->
-                            @if (session('impersonator'))
-                                <li class="dropdown-header">Impersonation</li>
-
-                                <!-- Stop Impersonating -->
-                                <li>
-                                    <a href="/app/kiosk/users/stop-impersonating">
-                                        <i class="fa fa-fw fa-btn fa-user-secret"></i>Back To My Account
-                                    </a>
-                                </li>
-
-                                <li class="divider"></li>
-                            @endif
-
-                            <!-- Developer -->
-                            @if (Auth::user()->email == 'tik.hakobyan@gmail.com')
-                                @include('nav.developer')
-                            @endif
-
-                            <li>
-                                <a href="/tigran">
-                                    My Profile
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/tigran/bookshelves">
-                                    My Bookshelves
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/settings">
-                                    Settings
-                                </a>
-                            </li>
-
-                            <li class="divider"></li>
-
-                            <!-- Logout -->
-                            <li>
-                                <a href="/logout">
-                                    Logout
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
                 </ul>
             </div>
         </div>
