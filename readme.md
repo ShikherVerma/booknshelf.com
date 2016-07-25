53761e887775ef56573699a0b2f8f1a94d89ac6a
Tigran Hakobyan <tik.hakobyan@gmail.com>
2016-07-24 22:25:41 -0400

    minor modal styling fixes

--- readme.md
+++ readme.md
@@ -2,12 +2,12 @@
 
 #### July 24th
 - Fix shelf search on landing
-- Fix the book search hover thing!!! finally!
 - Fix home page shelves display sql query. Display only our shelves.
 - Mixpanel tracking
 
 
 ### TODO Before Big Launch
+- Fix the book search hover thing!!! finally!
 - You should be able to save directly from "Most Saved Books" to your shelves
 - Users should be able to search for bookshelves even if they are not logged in...
 - Create all my own shelves
--- resources/views/profile/profile-liked-shelves.blade.php
+++ resources/views/profile/profile-liked-shelves.blade.php
@@ -1,8 +1,14 @@
 <app-profile-liked-shelves inline-template>
-    <div class="container max-width-1000 p-t-lg p-b-lg" >
-        <div class="alert alert-warning alert-dismissible hidden-xs" role="alert">
-          <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            Here you will see all the bookshelves you've liked. Coming very soon!
+    <div class="container max-width-1000 p-a">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="alert alert-warning hidden-xs" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h5 class="text-center">
+                        Here you will see all the bookshelves you've liked. Coming very soon!
+                    </h5>
+                </div>
+            </div>
         </div>
     </div>
 </app-profile-liked-shelves>
