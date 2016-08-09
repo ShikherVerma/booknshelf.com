88993af233ad1284a89c157af5435c7ee3d196e9
Tigran Hakobyan <tik.hakobyan@gmail.com>
2016-08-09 11:08:26 -0400

    Add social share buttons on profile page.

--- resources/views/profile/profile-header.blade.php
+++ resources/views/profile/profile-header.blade.php
@@ -8,6 +8,12 @@
                     {{ $user->about }}
                 </p>
             </div>
+            <p class="profile-header-bio">
+                <!-- Tweet your profile button -->
+                <a href="https://twitter.com/share" class="twitter-share-button" data-size="large" data-text="Check out my bookshelves on @booknshelf" data-show-count="false" data-url="https://booknshelf.com/{{'@'.$user->username}}">Tweet</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+                <iframe src="https://www.facebook.com/plugins/share_button.php?href=http%3A%2F%2Fbooknshelf.com/{{'@'.$user->username}}
+                &layout=button&size=large&mobile_iframe=true&appId=1899203000306326&width=73&height=28" width="73" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+            </p>
         </div>
 
         <nav class="profile-header-nav profile-index-tabs">
