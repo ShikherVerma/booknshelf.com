340278db637fa76968492fb453f54286254a8b4f
Tigran Hakobyan <tik.hakobyan@gmail.com>
2016-12-11 16:13:17 -0500

    another placeholder for moving booknshelf to vue 2.0.

--- resources/assets/js/components/shelf/ShelfBooks.vue
+++ resources/assets/js/components/shelf/ShelfBooks.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container m-y-md" data-grid="images">
+    <div class="container m-y-md shelf-books" data-grid="images">
         <div class="row">
             <shelf-book v-for="book in books" :book="book" :user="user"></shelf-book>
         </div>
