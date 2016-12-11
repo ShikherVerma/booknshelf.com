1f730e8563c0f1603b821623abea2cb81fa9d9a2
Tigran Hakobyan <tik.hakobyan@gmail.com>
2016-12-11 16:13:17 -0500

    another placeholder for moving booknshelf to vue 2.0.

--- resources/assets/js/components/Shelf.vue
+++ resources/assets/js/components/Shelf.vue
@@ -1,22 +1,24 @@
 <template>
-    <div class="profile-header shelf-page-header text-center" style="background-image: url('/img/backgrounds/shelf-page-background.jpg');">
+    <div class="profile-header shelf-page-header text-center">
         <div class="container">
             <div class="container-inner">
                 <h2 class="profile-header-user">{{ shelf.name }}</h2>
                 <p v-if="shelf.description" class="profile-header-bio">
                     {{ shelf.description}}
                 </p>
-                <h6 class="profile text-muted" :href="profileUrl">
+                <span>
                     <img class="img-circle small-profile-photo" :src="user.avatar">
-                    {{ user.name }}
-                </h6>
+                    <a class="profile-name" :href="profileUrl">
+                        {{ user.name }}
+                    </a>
+                </span>
             </div>
         </div>
 
         <nav class="profile-header-nav">
             <ul class="nav nav-tabs">
                 <li class="active">
-                    <a href="#">{{ books.length }} Books</a>
+                    <a href="#" class="books-count">{{ books.length }} Books</a>
                 </li>
             </ul>
         </nav>
@@ -36,4 +38,27 @@
 </script>
 
 <style lang="sass">
+    .shelf-page-header {
+        background-color: #F5F5F5
+    }
+
+    .profile-header-user {
+        color: #072B54;
+    }
+    .profile-header-bio {
+        color: lighten(#072B54, 5%);
+    }
+
+    .profile-name {
+        font-size: 14px;
+    }
+
+    .small-profile-photo {
+        height: 35px !important;
+        width: 35px !important;
+    }
+
+    .books-count {
+        color: black !important;
+    }
 </style>
