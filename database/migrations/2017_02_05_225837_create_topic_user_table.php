08d590b1c7ec8a6359830b1b96b540712b75c007
Tigran Hakobyan <tik.hakobyan@gmail.com>
2017-02-06 20:44:44 -0500

    fix the migration class

--- database/migrations/2017_02_05_225837_create_topic_user_table.php
+++ database/migrations/2017_02_05_225837_create_topic_user_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBookShelfTable extends Migration
+class CreateTopicUserTable extends Migration
 {
     /**
      * Run the migrations.
