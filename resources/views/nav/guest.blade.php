4619332df73877e128b484c57d02d75bc57e6b46
Tigran Hakobyan <tik.hakobyan@gmail.com>
2017-02-14 08:35:47 -0500

    Small update.

--- resources/views/nav/guest.blade.php
+++ resources/views/nav/guest.blade.php
@@ -7,9 +7,9 @@
             </span>
             <span style="margin-left: 4px;">Home</span>
         </a>
-        <a class="nav-item" style="font-weight: normal;" href="/about">
-            About
-        </a>
+        {{--<a class="nav-item" style="font-weight: normal;" href="/about">--}}
+            {{--About--}}
+        {{--</a>--}}
         <a class="nav-item" href="/topics">
             Topics
         </a>
