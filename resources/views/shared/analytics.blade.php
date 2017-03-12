1d7be80964776378951e6755e06e9d80d206c8da
Tigran Hakobyan <tik.hakobyan@gmail.com>
2017-03-12 21:28:12 +0100

    Small fixes and do not load FB and twitter scripts in analytics file. No need.

--- resources/views/shared/analytics.blade.php
+++ resources/views/shared/analytics.blade.php
@@ -1,14 +1,14 @@
-<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+{{--<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>--}}
 
-<!-- Load Facebook SDK for JavaScript -->
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=1899203000306326";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+{{--<!-- Load Facebook SDK for JavaScript -->--}}
+{{--<div id="fb-root"></div>--}}
+{{--<script>(function(d, s, id) {--}}
+  {{--var js, fjs = d.getElementsByTagName(s)[0];--}}
+  {{--if (d.getElementById(id)) return;--}}
+  {{--js = d.createElement(s); js.id = id;--}}
+  {{--js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=1899203000306326";--}}
+  {{--fjs.parentNode.insertBefore(js, fjs);--}}
+{{--}(document, 'script', 'facebook-jssdk'));</script>--}}
 
 <script async>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
