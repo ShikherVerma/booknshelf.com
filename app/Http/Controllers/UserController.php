c99508d3705f707152a6e9892213aa95b6f9dcc3
Tigran Hakobyan <tik.hakobyan@gmail.com>
2017-03-19 17:10:39 -0400

    You don't have to login to see the followings in the profile page.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,6 +23,7 @@ class UserController extends Controller
                 'profile',
                 'allShelves',
                 'allFollowers',
+                'allFollowed',
                 'shelf',
             ],
         ]);
