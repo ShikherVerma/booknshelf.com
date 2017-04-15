13868efe32e40a2023a3854525cf5ff34cc610d6
Tigran Hakobyan <tik.hakobyan@gmail.com>
2017-04-15 17:26:25 -0400

    People can now contribute to shelves.

--- resources/views/bookshelves.blade.php
+++ resources/views/bookshelves.blade.php
@@ -16,6 +16,21 @@
 <section id="favorite-shelves" class="section is-primary is-bold" v-cloak>
     <div class="container">
         <div class="columns is-multiline">
+            <div class="column is-3">
+                <div class="content">
+                    <p class="subtitle has-text-centered" style="margin-top:15%;">
+                        <strong class="primary-span">
+                            Would you like to feature your shelf here?
+                        </strong>
+                    </p>
+                    <p class="has-text-centered">
+                        <a href="https://goo.gl/forms/riwJ6CcbVbvPUOZl1" type="button" target="_blank"
+                           class="button is-primary is-large hvr-glow">
+                            <span><strong>Feature my bookshelf</strong></span>
+                        </a>
+                    </p>
+                </div>
+            </div>
             @foreach ($shelves as $shelf)
                 <div class="column is-3">
                     <a href="{{ route('shelf_path', ['username' => $shelf['user']['username'], 'shelf_slig' =>$shelf['slug']]) }}">
