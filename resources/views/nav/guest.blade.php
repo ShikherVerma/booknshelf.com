8dbcbb803f63c8f99601031da76a833f3ec68bc7
Tigran Hakobyan <tik.hakobyan@gmail.com>
2017-06-09 18:18:00 -0400

    Fix amazon book search

--- resources/views/nav/guest.blade.php
+++ resources/views/nav/guest.blade.php
@@ -9,9 +9,9 @@
         <a class="nav-item" href="/faq">
             <span>FAQ</span>
         </a>
-        <!-- <a class="nav-item" href="/story">
+        <a class="nav-item" href="/story">
             <span>Blog</span>
-        </a> -->
+        </a>
         <a class="nav-item" href="/topics">
             Topics
         </a>
