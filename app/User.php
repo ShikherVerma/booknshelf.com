2b28de68b0d80741b3a6ac77dd277c4290710380
Tigran Hakobyan <tik.hakobyan@gmail.com>
2017-06-14 22:30:04 -0400

    fix the tests

--- app/User.php
+++ app/User.php
@@ -5,10 +5,11 @@ namespace App;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Collection;
+use Laravel\Scout\Searchable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, Searchable;
     /**
      * The attributes that are mass assignable.
      *
