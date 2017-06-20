1ca00ab291534d2c8303a63e0dd6e0d823f5d854
Tigran Hakobyan <tik.hakobyan@gmail.com>
2017-06-20 16:07:15 -0400

    Update booknshelf's main pitch.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,14 +7,14 @@
     <meta name="google-site-verification" content="oRlwQCWDSEBrMAdKd3t3_3pCA9sFayLgZdgLer2TKfM"/>
     <link rel="canonical" href="{{ url()->current() }}"/>
     <meta name="description"
-          content="People come to Booknshelf to create their online reading journey by organizing their books in shelves."/>
+          content="Easily organize all your books in different shelves."/>
     <meta name="google-site-verification" content="oRlwQCWDSEBrMAdKd3t3_3pCA9sFayLgZdgLer2TKfM" />
 
     {{--Facebook meta tags--}}
     <meta property="fb:app_id" content="1899203000306326"/>
-    <meta property="og:title" content="{{ $title or 'Stay organized with your books & read more' }}"/>
+    <meta property="og:title" content="{{ $title or 'Keep track of all your books online' }}"/>
     <meta property="og:description"
-          content="{{ $description or 'People come to Booknshelf to create their online reading journey by organizing their books in shelves.' }}"/>
+          content="{{ $description or 'Easily organize all your books in different shelves.' }}"/>
     <meta property="og:image" content="{{ $ogImage or 'https://booknshelf.com/img/social/homepage-screenshot-social-main-new.png' }}"/>
     <meta property="og:image:height" content="717"/>
     <meta property="og:image:width" content="1200"/>
@@ -25,17 +25,17 @@
     {{--Twitter meta tags--}}
     <meta name="twitter:card" content="summary">
     <meta name="twitter:description"
-          content="{{ $description or 'People come to Booknshelf to create their online reading journey by organizing their books in shelves.' }}">
+          content="{{ $description or 'Easily organize all your books in different shelves.' }}">
     <meta name="twitter:image"
           content="{{ $ogImage or 'https://booknshelf.com/img/social/homepage-screenshot-social-main-new.png' }}">
     <meta name="twitter:site" content="@booknshelf">
     <meta name="twitter:title"
-          content="{{ $title or 'Stay organized with your books & read more' }}">
+          content="{{ $title or 'Keep track of all your books online' }}">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ $title or 'Booknshelf' }}</title>
+    <title>{{ $title or 'Booknshelf: Keep track of all your books online.' }}</title>
     <link rel="icon" type="image/png" href="/img/favicons/favicon-16x16.png" sizes="16x16">
     <link rel="icon" type="image/png" href="/img/favicons/favicon-32x32.png" sizes="32x32">
     <link rel="icon" type="image/png" href="/img/favicons/favicon-96x96.png" sizes="96x96">
