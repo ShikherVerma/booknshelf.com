7cf053ec185d3a4ba78647d8946acc831e4bcbc5
Tigran Hakobyan <tik.hakobyan@gmail.com>
2017-06-26 23:42:57 -0400

    Remove some files that are not needed for Goodreads API

--- app/Services/Goodreads/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('CONSUMER_KEY', 'your goodreads key');
-define('CONSUMER_SECRET', 'your goodreads secret key');
-define('CALLBACK_URL', 'http://yourURL.com/goodreads/callback.php');
-
-
-?>
