4496be842637957bdc6ef81f0fef492ecaf0542a
Tigran Hakobyan <tik.hakobyan@gmail.com>
2017-07-20 16:40:40 -0400

    Create the UI of writing notes in book info modal

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -1,18 +1,4 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="section" style="background-color: #fafafa;">
-        <div class="container">
-            <div class="columns">
-                <book-view-left :book="{{ $book }}" :user="{{ $user }}" :likes="userLikedBooks" :saves="userSavedBooks"></book-view-left>
-                <book-view-right :book="{{ $book }}"></book-view-right>
-                <book-view-center :book="{{ $book }}" :user="{{ $user }}"></book-view-center>
-            </div>
-        </div>
-    </section>
-
-
-    @if (Auth::check())
-        <book-save-modal :user="{{ $user }}" :book="bookSaveModalBook" :show="bookSaveModal"></book-save-modal>
-    @endif
 @endsection
