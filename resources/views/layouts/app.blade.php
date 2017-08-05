23fa1a270f4c1358e62e7d777d69ba049bee677d
Tigran Hakobyan <tik.hakobyan@gmail.com>
2017-08-05 18:59:56 -0400

    Add google adsense to booknshelf

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,6 +60,13 @@
         ]); ?>
     </script>
     @include('shared.analytics')
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+      (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-1090440830384862",
+        enable_page_level_ads: true
+      });
+    </script>
 </head>
 <body>
 <div id="app">
